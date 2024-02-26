86dd7f11d37b6b51e6c1451dab618384cbbd6a4b
Danesh <Danesh@192.168.2.15>
2024-02-27 00:22:43 +0530

    application

--- RestApiPractices/src/main/java/com/danunaik/firstspr/RestApiPracticesApplication.java
+++ RestApiPractices/src/main/java/com/danunaik/firstspr/RestApiPracticesApplication.java
@@ -1,13 +1,19 @@
 package com.danunaik.firstspr;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RestApiPracticesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiPracticesApplication.class, args);
+	ApplicationContext context=	SpringApplication.run(RestApiPracticesApplication.class, args);
+		
+	
+	
+		
 	}
 
 }
