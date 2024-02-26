b06cd50558eedaf43895ee6f87cba0daa23424d0
Danesh <Danesh@192.168.2.15>
2024-02-27 00:08:42 +0530

    pushing

---
+++ RestApiPractices/src/test/java/com/danunaik/firstspr/RestApiPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danunaik.firstspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
